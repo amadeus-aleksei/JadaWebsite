2f8b0f56a2f8052b9a0eaf7cf90304b7e93497f4
amadeus-aleksei <amadeusaleksei@gmail.com>
2024-02-26 21:44:09 -0600

    Fixing

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'JadaWebsite'
-})
+  base: "JadaWebsite",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
+});
